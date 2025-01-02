994db6c75be80ecb1f3c52868f47df3eecc73e7a
laith_waseem <hayder.hayder2010@gmail.com>
2025-01-03 01:04:03 +0300

    b0.4

--- tvos/tvosmain.js
+++ tvos/tvosmain.js
@@ -3,10 +3,16 @@ let wlctJS = document.getElementById("wlct");
 const wlcnsJS = document.getElementById("wlcns");
 const wlcnbJS = document.getElementById("wlcnb");
 
+
 function updateName() {
-    wlctJS.innerHTML = `welcome back, ${namiJS.value} `;
+    localStorage.setItem("nami", namiJS.value);
+    wlctJS.innerHTML = `welcome back, ${localStorage.getItem("nami")} `;
     wlcnbJS.innerHTML = "";
 }
 
 wlcnsJS.addEventListener("click", updateName)
 
+if (localStorage.getItem("nami")) {
+    wlctJS.innerHTML = `welcome back, ${localStorage.getItem("nami")} `;
+    wlcnbJS.innerHTML = "";
+}
